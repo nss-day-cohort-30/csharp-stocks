762fc982c535782a4c18ef91bdf26546801a9901
Steve Brownlee <chortlehoort@gmail.com>
2019-04-08 15:41:36 -0500

    Better formatting

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace dictionaries {
+namespace StockReport {
     class Program {
         static void Main (string[] args) {
-            Dictionary<string, string> stocks = new Dictionary<string, string> () { { "GM", "General Motors" }, { "GE", "General Electric" }, { "CAT", "Caterpillar" }, { "AAPL", "Apple" }, { "MSFT", "Microsoft" },
+            Dictionary<string, string> stocks = new Dictionary<string, string> () {
+                { "GM", "General Motors" },
+                { "GE", "General Electric" },
+                { "CAT", "Caterpillar" },
+                { "AAPL", "Apple" },
+                { "MSFT", "Microsoft" },
             };
 
             Dictionary<string, List<double>> purchases = new Dictionary<string, List<double>> ();
@@ -15,8 +20,6 @@ namespace dictionaries {
             purchases.Add ("MSFT", new List<double> () { 43908, 9823.21 });
             purchases.Add ("GE", new List<double> () { 3259.94, 3402.24 });
             purchases.Add ("GM", new List<double> () { 390.2, 385.90, 7845.23, 4598.29 });
-
-            Dictionary<string, double> stockReport = new Dictionary<string, double> ();
 
             /*
                Iterate over the purchases and record the valuation
